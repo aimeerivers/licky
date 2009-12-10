404d43dce03bc1323507feaa9c186798611b859d
Aimee Daniells <aimee@edendevelopment.co.uk>
2009-12-10 13:17:31 +0000

    Enable adding content to a page.

--- page.rb
+++ page.rb
@@ -1,5 +1,6 @@
 class Page
   attr_reader :title
+  attr_accessor :content
 
   def initialize(title)
     @title = title
@@ -17,7 +18,14 @@ class PageTest
     p = Page.new('O HAI')
     assert_equal(p.title, 'O HAI')
   end
+
+  def page_has_content
+    p = Page.new('With Content')
+    p.content = 'Lorem ipsum'
+    assert_equal(p.content, 'Lorem ipsum')
+  end
 end
 
 test = PageTest.new
 test.creating_a_new_page
+test.page_has_content
