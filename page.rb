0af0c62e46e7e1cb382841e4aee233fcb8e4c221
aimee daniells <aimee@mychores.co.uk>
2009-12-13 08:59:39 +0000

    Method for figuring out filenames.
    Factor out the test helper so that it can be included in multiple test classes.

--- page.rb
+++ page.rb
@@ -12,13 +12,20 @@ class PageFactory
   def self.find_or_create(title)
     Page.new(title)
   end
+
+  def self.filename_for(title)
+    title.gsub(' ', '_') + '.txt'
+  end
 end
 
-class PageTest
-
+module TestHelper
   def assert_equal(actual, expected)
     puts actual == expected
   end
+end
+
+class PageTest
+  include TestHelper
 
   def creating_a_new_page_using_the_factory
     p = PageFactory.find_or_create('O HAI')
@@ -37,7 +44,27 @@ class PageTest
   end
 end
 
+class PageFactoryTest
+  include TestHelper
+
+  def figuring_out_filename_with_just_one_word
+    filename = PageFactory.filename_for('First')
+    assert_equal(filename, 'First.txt')
+  end
+
+  def figuring_out_filename_with_more_than_one_word
+    filename = PageFactory.filename_for('Two Words')
+    assert_equal(filename, 'Two_Words.txt')
+  end
+
+end
+
+
 test = PageTest.new
 test.creating_a_new_page_using_the_factory
 test.page_has_no_content_to_start_with
 test.page_can_be_given_content
+
+test = PageFactoryTest.new
+test.figuring_out_filename_with_just_one_word
+test.figuring_out_filename_with_more_than_one_word
